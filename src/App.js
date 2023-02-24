27972137e7db5ca442f7922509c1df1d081b3913
piyali2016 <piyali.acn@gmail.com>
2023-02-24 18:13:19 +0530

    Add files via upload
    assignment 4

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CouseList from './CouseList';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Course List</h1>
+    <div>
+    <CouseList></CouseList>
+    </div>
+     
+    </div>
+  );
+}
+
+export default App;
